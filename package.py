6b7827a046d941d5bfb021c724b469545fddab09
YoussefMohamed-joe <mohammedyoussef111@gmail.com>
2026-06-04 11:02:09 +0300

    version up

--- package.py
+++ package.py
@@ -1,7 +1,7 @@
 """3DEqualizer addon package definition."""
 name = "equalizer"
 title = "3DEqualizer"
-version = "0.2.0+rankV2"
+version = "0.2.0+rankV3"
 app_host_name = "equalizer"
 client_dir = "ayon_equalizer"
 project_can_override_addon_version = True
